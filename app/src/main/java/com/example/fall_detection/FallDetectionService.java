8094ef47c92c6f4493e54bf21259a3a7d69ec8d9
FaezeShakuri <f.shakori.com@gmail.com>
2022-01-26 16:35:10 +0330

    test mode added

--- app/src/main/java/com/example/fall_detection/FallDetectionService.java
+++ app/src/main/java/com/example/fall_detection/FallDetectionService.java
@@ -11,9 +11,26 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.concurrent.TimeUnit;
+
 public class FallDetectionService extends Service implements SensorEventListener {
+
+    private boolean testMod = false;
+
     private SensorManager sensorManager;
     private Sensor accelerometer_sensor, gyroscope_sensor;
+
+    private JSONArray sensorTestData = null;
 
     private float[] accelerometerReading = new float[3];
     private float[] gyroscopeReading = new float[3];
@@ -44,8 +61,15 @@ public class FallDetectionService extends Service implements SensorEventListener
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        return;
+    public void onCreate() {
+        super.onCreate();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        initSensors();
+//        changeSamplingRate(sensorAccuracy);
     }
 
     private void detection() {
@@ -70,19 +94,56 @@ public class FallDetectionService extends Service implements SensorEventListener
                 Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    private void writeToFile(String data, Context context){
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("testLog.txt", Context.MODE_APPEND));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
 
-        if (sensorEvent == null)
-            return;
+    public static String readFromFile(String file, Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(file);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    private void run(int sensorType, float[] sensorData) {
 
         long time = System.currentTimeMillis();
 
         if (!isACCLessthanLFT) {
-            if (sensorEvent.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+            if (sensorType != Sensor.TYPE_ACCELEROMETER)
                 return;
 
-            float _acc = (float) Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] + sensorEvent.values[2] * sensorEvent.values[2]);
+            float _acc = (float) Math.sqrt(sensorData[0] * sensorData[0] + sensorData[1] * sensorData[1] + sensorData[2] * sensorData[2]);
             if (_acc < accLFT) {
                 isACCLessthanLFT = true;
                 accelerometerTime = time;
@@ -96,13 +157,13 @@ public class FallDetectionService extends Service implements SensorEventListener
                 return;
             }
 
-            switch (sensorEvent.sensor.getType()) {
+            switch (sensorType) {
                 case Sensor.TYPE_ACCELEROMETER:
-                    acc = (float) Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] + sensorEvent.values[2] * sensorEvent.values[2]);
+                    acc = (float) Math.sqrt(sensorData[0] * sensorData[0] + sensorData[1] * sensorData[1] + sensorData[2] * sensorData[2]);
                     break;
 
                 case Sensor.TYPE_GYROSCOPE:
-                    gyro = (float) Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] + sensorEvent.values[2] * sensorEvent.values[2]);
+                    gyro = (float) Math.sqrt(sensorData[0] * sensorData[0] + sensorData[1] * sensorData[1] + sensorData[2] * sensorData[2]);
                     break;
             }
 
@@ -120,7 +181,7 @@ public class FallDetectionService extends Service implements SensorEventListener
             }
 
             // First time fall detected, call detection function
-            if (!fallDetected)  {
+            if (!fallDetected) {
                 timeDetected = time;
                 fallDetected = true;
                 acc = gyro = -1;
@@ -136,19 +197,35 @@ public class FallDetectionService extends Service implements SensorEventListener
 
             acc = gyro = -1;
             isACCLessthanLFT = false;
-//            Log.i("FALL", "Fall Detected");
-//            Toast.makeText(getApplicationContext(),"fall detected",Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void initSensors() {
-        sensorManager.registerListener(this,
-                accelerometer_sensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        return;
+    }
 
-        sensorManager.registerListener(this,
-                gyroscope_sensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent == null)
+            return;
+        if (!testMod)
+            run(sensorEvent.sensor.getType(), sensorEvent.values);
+    }
+
+    private void initSensors() {
+
+        if (!testMod) {
+            sensorManager.registerListener(this,
+                    accelerometer_sensor,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+
+            sensorManager.registerListener(this,
+                    gyroscope_sensor,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            simulateSensors();
+        }
     }
 
     public void changeSamplingRate(int i) {
@@ -174,17 +251,40 @@ public class FallDetectionService extends Service implements SensorEventListener
 
     }
 
+    private void loadSensorTestData() {
+        try {
+            sensorTestData = new JSONArray(fall);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    private void simulateSensors() {
+        if (sensorTestData == null)
+            return;
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometer_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyroscope_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        for (int i = 0; i < sensorTestData.length(); i++) {
+            JSONObject sensorDataElement = sensorTestData.optJSONObject(i);
+            try {
+                int type = sensorDataElement.getInt("type");
+                float x = (float) sensorDataElement.getDouble("x");
+                float y = (float) sensorDataElement.getDouble("y");
+                float z = (float) sensorDataElement.getDouble("z");
+                run(type, new float[]{x, y, z});
 
-//        initSensors();
-        changeSamplingRate(sensorAccuracy);
+                if (i <= sensorDataElement.length() - 2) {
+                    long time = sensorDataElement.getLong("time");
+                    JSONObject sensorNextDataElement = sensorTestData.optJSONObject(i + 1);
+                    long nextTime = sensorNextDataElement.getLong("time");
+                    TimeUnit.MILLISECONDS.sleep(nextTime - time);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Nullable
@@ -200,5 +300,7 @@ public class FallDetectionService extends Service implements SensorEventListener
     public static String getGlobal_number() {
         return global_number;
     }
+
+    String fall = "[{\"time\":1643188284044,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284054,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284064,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284076,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284084,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284094,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284105,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284114,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284126,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284134,\"type\":1,\"x\":0.07999999821186066,\"y\":6.228013038635254,\"z\":6.961212158203125},{\"time\":1643188284135,\"type\":4,\"x\":0,\"y\":0,\"z\":0},{\"time\":1643188284136,\"type\":1,\"x\":-0.07999999821186066,\"y\":6.213071823120117,\"z\":6.940082550048828},{\"time\":1643188284143,\"type\":1,\"x\":-0.03999999910593033,\"y\":6.190186500549316,\"z\":6.921594142913818},{\"time\":1643188284145,\"type\":1,\"x\":0,\"y\":6.165256023406982,\"z\":6.905416965484619},{\"time\":1643188284146,\"type\":1,\"x\":0.03999999910593033,\"y\":6.153253555297852,\"z\":6.896167755126953},{\"time\":1643188284148,\"type\":4,\"x\":0,\"y\":0,\"z\":0}]";
 
 }
