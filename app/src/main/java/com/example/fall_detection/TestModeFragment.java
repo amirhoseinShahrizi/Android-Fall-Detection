04db76224a6a0c36097ff7f1fff6387ece5d9ba5
FaezeShakuri <f.shakori.com@gmail.com>
2022-01-26 16:45:48 +0330

    upload file

--- app/src/main/java/com/example/fall_detection/TestModeFragment.java
+++ app/src/main/java/com/example/fall_detection/TestModeFragment.java
@@ -1,8 +1,13 @@
 package com.example.fall_detection;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,8 +17,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,6 +30,14 @@ import java.util.ArrayList;
  */
 public class TestModeFragment extends Fragment {
 
+    private static final int MY_REQUEST_CODE_PERMISSION = 1000;
+    private static final int MY_RESULT_CODE_FILECHOOSER = 2000;
+
+    private Button buttonBrowse;
+    private EditText editTextPath;
+
+    private static final String LOG_TAG = "AndroidExample";
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,9 +46,6 @@ public class TestModeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    EditText records_ET;
-    ImageButton clear_IB;
 
     public TestModeFragment() {
         // Required empty public constructor
@@ -69,19 +80,117 @@ public class TestModeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        View A = inflater.inflate(R.layout.fragment_test_mode, container, false);
+        View A = inflater.inflate(R.layout.fragment_test_mode_2, container, false);
         setRV(A);
-
-        records_ET = A.findViewById(R.id.test_mode_et);
-        clear_IB = A.findViewById(R.id.clear_ib);
-        clearClicked(clear_IB);
-
-
         return A;
     }
 
+    private void askPermissionAndBrowseFile()  {
+        // With Android Level >= 23, you have to ask the user
+        // for permission to access External Storage.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // Level 23
+
+            // Check if we have Call permission
+            int permisson = ActivityCompat.checkSelfPermission(this.getContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permisson != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_REQUEST_CODE_PERMISSION
+                );
+                return;
+            }
+        }
+        this.doBrowseFile();
+    }
+
+    private void doBrowseFile()  {
+        Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFileIntent.setType("*/*");
+        // Only return URIs that can be opened with ContentResolver
+        chooseFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        Bundle bundle = null;
+
+        chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose a file");
+        ActivityCompat.startActivityForResult(this.getActivity(), chooseFileIntent, MY_RESULT_CODE_FILECHOOSER, bundle);
+    }
+
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case MY_REQUEST_CODE_PERMISSION: {
+
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (CALL_PHONE).
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(LOG_TAG, "Permission granted!");
+                    Toast.makeText(this.getContext(), "Permission granted!", Toast.LENGTH_SHORT).show();
+
+                    this.doBrowseFile();
+                }
+                // Cancelled or denied.
+                else {
+                    Log.i(LOG_TAG, "Permission denied!");
+                    Toast.makeText(this.getContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case MY_RESULT_CODE_FILECHOOSER:
+                if (resultCode == Activity.RESULT_OK ) {
+                    if(data != null)  {
+                        Uri fileUri = data.getData();
+                        Log.i(LOG_TAG, "Uri: " + fileUri);
+
+                        String filePath = null;
+                        try {
+                            filePath = FileUtils.getPath(this.getContext(),fileUri);
+                        } catch (Exception e) {
+                            Log.e(LOG_TAG,"Error: " + e);
+                            Toast.makeText(this.getContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                        }
+                        this.editTextPath.setText(filePath);
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public String getPath()  {
+        return this.editTextPath.getText().toString();
+    }
+
     private void setRV(View view){
+
+        this.editTextPath = (EditText) view.findViewById(R.id.editText_path);
+        this.buttonBrowse = (Button) view.findViewById(R.id.button_browse);
+
+        this.buttonBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askPermissionAndBrowseFile();
+            }
+        });
+
+
         //setting recycler view
         //later get from database probably
 
@@ -100,22 +209,5 @@ public class TestModeFragment extends Fragment {
         FallCardAdapter fallCardAdapter_fd = new FallCardAdapter(falls_list);
         recyclerView_fd.setAdapter(fallCardAdapter_fd);
         recyclerView_fd.setItemAnimator(new DefaultItemAnimator());
-    }
-
-    private void clearClicked(ImageButton ib){
-        ib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                if (toggle_btn.isChecked()) {
-//                    Log.i("Start", "Start Clicked");
-//                    getActivity().startService(new Intent(getActivity(), FallDetectionService.class));
-//                }
-//                else {
-//                    Log.i("Stop", "Stop Clicked");
-//                    getActivity().stopService(new Intent(getActivity(), FallDetectionService.class));
-//                }
-                records_ET.setText("");
-            }
-        });
     }
 }
